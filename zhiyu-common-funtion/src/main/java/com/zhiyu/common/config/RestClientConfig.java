c37baf7d6481686347d0a820f65e0aa8538c33da
Jason Weng <wzy7627@gmail.com>
2021-08-27 18:49:07 +0800

    update springboot version

---
+++ zhiyu-common-funtion/src/main/java/com/zhiyu/common/config/RestClientConfig.java
@@ -0,0 +1,12 @@
+//package com.zhiyu.common.config;
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.data.elasticsearch.config.AbstractElasticsearchConfiguration;
+//
+///**
+// * @author wengzhiyu
+// * @since 2021/8/27 17:52
+// */
+//@Configuration
+//public class RestClientConfig extends AbstractElasticsearchConfiguration {
+//}
